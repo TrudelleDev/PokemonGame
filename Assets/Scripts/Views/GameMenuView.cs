f6809be21a8fea31f8796b91559aca6faf592f06
Alexandre Trudelle <trudellealexandredev@gmail.com>
2025-06-17 18:58:56 -0400

    remove TrainerCard and Pokedex UI components
    Removed unused systems to simplify the project and reduce scope.

--- Assets/Scripts/Views/GameMenuView.cs
+++ Assets/Scripts/Views/GameMenuView.cs
@@ -6,14 +6,12 @@ namespace PokemonGame.Views
     {
         [SerializeField] private MenuButton party;
         [SerializeField] private MenuButton inventory;
-        [SerializeField] private MenuButton trainerCard;
         [SerializeField] private MenuButton exit;
 
         public override void Initialize()
         {
             party.OnClick += () => ViewManager.Instance.Show<PartyMenuView>();
             inventory.OnClick += () => ViewManager.Instance.Show<BagView>();
-            trainerCard.OnClick += () => ViewManager.Instance.Show<TrainerCardView>();
             exit.OnClick += () => ViewManager.Instance.GoToPreviousView();
         }
     }
